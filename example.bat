487c9c210435a2622214fdcf857074553d4d06d6
Jack O'Connor <oconnor663@gmail.com>
2015-12-14 00:25:07 -0500

    maximum simplification

--- example.bat
+++
@@ -1,7 +0,0 @@
-echo running test.py directly
-
-test.py
-
-echo running it with python
-
-python test.py
--- second.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python3
-
-import sys
-
-print("running second.py")
-print("sys.executable", sys.executable)
