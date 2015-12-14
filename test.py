4bc14f8831a289ab16c9afc7800dd14d926758e7
Jack O'Connor <oconnor663@gmail.com>
2015-12-13 23:24:14 -0500

    make executable and convert bytes

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ print("trying to launch second.py through the shell")
 subprocess.call("second.py", shell=True)
 
 print("doing it again and capturing output")
-out = subprocess.check_output("second.py", shell=True)
+out = subprocess.check_output("second.py", shell=True).decode()
 print("here's the output:", repr(out))
 assert os.environ['PYTHON'] in out, "look for the PYTHON path in there"
