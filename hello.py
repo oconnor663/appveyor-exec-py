8803acb912f9d7902300a3c072ef9ca8dad52a6f
Jack O'Connor <oconnor663@gmail.com>
2015-12-21 23:21:02 -0500

    run under Python 3

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 #! /usr/bin/env python3
 
-print("OH MY GOSH ITS WORKING")
+import sys
+
+# We deliberately use Python-3-only print flags, to make sure we're running
+# under Python 3.
+print("OH MY GOSH ITS WORKING", sys.version, file=sys.stderr)
